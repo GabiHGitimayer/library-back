3ead06158cb596337cbc155731930fd9ec9e15f4
nicolasishizawa <nicolaskaiishizawa@gmail.com>
2024-12-05 20:53:07 -0300

    Revert "Changing seeders and uploading them to Git lessons"
    This reverts commit a09f75648590548297810bd9e26a5c1f0c61aab4.

--- src/main/java/com/biblioteca/repositories/UserRepository.java
+++ src/main/java/com/biblioteca/repositories/UserRepository.java
@@ -8,5 +8,4 @@ import com.biblioteca.entities.UserEntity;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     UserDetails findByUserCpf(String userCpf);
-    boolean existsByEmail(String email);
 }
