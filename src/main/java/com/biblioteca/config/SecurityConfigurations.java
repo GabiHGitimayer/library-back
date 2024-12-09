7511f72c1e7289e05fb22e3fb9c159d8734b6127
nicolasishizawa <nicolaskaiishizawa@gmail.com>
2024-12-09 17:50:30 -0300

    Adding auth in register endpoint

--- src/main/java/com/biblioteca/config/SecurityConfigurations.java
+++ src/main/java/com/biblioteca/config/SecurityConfigurations.java
@@ -31,7 +31,6 @@ public class SecurityConfigurations {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource())) 
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
